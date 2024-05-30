fe046e7d1ec36ca80095fa717e3af0e708c969e1
aliefdany <aliefseventri@gmail.com>
2024-05-30 10:48:40 +0700

    moved migrations from old repo

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('nim',15)->default('');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
