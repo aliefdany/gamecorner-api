c681b9eb790eed63c1b3a0b4d4b9780339ab2e62
aliefdany <aliefseventri@gmail.com>
2024-05-30 10:51:32 +0700

    moved requests from old repo

---
+++ app/Http/Requests/StoreOrderRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        // simple authorization rule, will improve later
+        return $this->user() != null;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'controller_amount' => 'bail|required|numeric|min:1|max:2'
+        ];
+    }
+}
